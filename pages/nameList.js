ea88e2da1408710802b9c79b74dec1f1f9e73651
Chandan <chandancvp08@gmail.com>
2022-03-12 05:27:28 +0530

    updated the list

--- pages/nameList.js
+++ pages/nameList.js
@@ -2,9 +2,17 @@ import React from 'react'
 import getSheetData from '../getSheetData'
 
 function nameList() {
-    const {A,B,C,D} = getSheetData()
+    const {A,B,C,D,rand} = getSheetData()
     return (
         <div className="p-5">
+            <h1 className='text-4xl mt-10'>total count : {rand.length}</h1>
+            {
+                rand.map((ele)=> <p key={ele}>{ele}</p>)
+            }
+            
+            
+            
+            <h1 className='text-4xl mt-10'>Class wise (maybe a bit inaccurate): </h1>
             <h1 className='text-4xl mt-10'>Total count:{A.length+B.length+C.length+D.length}</h1>
             
             <h1 className='text-4xl mt-10'>A section</h1>
