c03c79f15d7449099ee9e95d89c211fa220b8c6b
Chandan <chandancvp08@gmail.com>
2022-03-12 16:36:34 +0530

    updated the list for the last timeeee lmaooo

--- pages/nameList.js
+++ pages/nameList.js
@@ -10,30 +10,6 @@ function nameList() {
                 rand.map((ele)=> <p key={ele}>{ele}</p>)
             }
             
-            
-            
-            <h1 className='text-4xl mt-10'>Class wise (maybe a bit inaccurate): </h1>
-            <h1 className='text-4xl mt-10'>Total count:{A.length+B.length+C.length+D.length}</h1>
-            
-            <h1 className='text-4xl mt-10'>A section</h1>
-            {
-                A.map((ele)=> <p key={ele}>{ele}</p>)
-            }
-
-
-            <h1 className='text-4xl mt-10'>B section</h1>
-            {
-                B.map((ele)=> <p key={ele} >{ele}</p>)
-            }
-            <h1 className='text-4xl mt-10'>C section</h1>
-            {
-                C.map((ele)=> <p key={ele}>{ele}</p>)
-            }
-            <h1 className='text-4xl mt-10'>D section</h1>
-            {
-                D.map((ele)=> <p key={ele}>{ele}</p>)
-            }
-        
         </div>
   )
 }
